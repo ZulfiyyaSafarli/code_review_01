f0f6baa830709de30aee7ed042d91fa2230acf7d
ZulfiyyaSafarli <129965508+ZulfiyyaSafarli@users.noreply.github.com>
2026-09-23 19:38:50 +0400

    onClick and drawPixel functions

--- bresenham.js
+++ bresenham.js
@@ -9,6 +9,14 @@ var context;
 window.onload = init;
 var GRID_SIZE = 30;
 var GRID_COLOR = "grey";
+var LINE_COLOR = "red";
+//Coordinates of the line to draw
+var lineX0, lineY0;
+var lineX1, lineY1;
+
+//Remember if you are to draw a line or mark the start point
+var startNewLine = false; 
+
 function init() {
     // Get reference to the 2d context of the canvas
     canvas = document.getElementById("gl-canvas");
@@ -17,7 +25,46 @@ function init() {
     if (context) {
         console.log("Context created");
         drawGrid();
+        // set a listener for the mouse down event
+        canvas.onclick = onClick;
     }
+}
+function onClick(event) {
+    startNewLine = !startNewLine;
+    // Get the coordinates of the mouse click witin the canvas
+    rect = canvas.getBoundingClientRect();
+    x = event.clientX - rect.left;
+    y = event.clientY - rect.top;
+
+    // console.log(x, ':', y);
+
+    // Calculate the coordinates within the grid
+    x = Math.floor(x / GRID_SIZE);
+    y = Math.floor(y / GRID_SIZE);
+
+    if (startNewLine) {
+        // draw the first pixel
+        lineX0 = x;
+        lineY0 = y;
+        drawPixel(lineX0, lineY0);
+    } else {
+        // draw the line to the second pixel
+        lineX1 = x;
+        lineY1 = y;
+        drawBresenham();
+    }
+
+}
+
+function drawPixel(x, y) {
+    context.fillStyle = LINE_COLOR;
+    x = x * GRID_SIZE;
+    y = y * GRID_SIZE;
+    context.fillRect(x, y, GRID_SIZE, GRID_SIZE);
+}
+
+function drawBresenham() {
+    
 }
 function drawLine(x0, y0, x1, y1) {
     context.beginPath();
@@ -38,7 +85,7 @@ function drawGrid() {
         drawLine(x, 0, x, canvas.clientHeight);
         x += GRID_SIZE;
     }
-    
+
     //Draw the horizontal lines
     y = 0;
     while (y < canvas.clientHeight) {
